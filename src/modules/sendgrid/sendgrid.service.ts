35bc27020ea16855286cbbe15fd9a9a5e0ddca1a
Minchuk Ilya <cacatubutyt@gmail.com>
2024-09-01 18:44:42 +0200

    refactor: remove extra import renaming in sendgid.service.ts

--- src/modules/sendgrid/sendgrid.service.ts
+++ src/modules/sendgrid/sendgrid.service.ts
@@ -1,19 +1,15 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import {
-  MailService as SendGridMailService,
-  ClientResponse,
-  MailDataRequired,
-} from '@sendgrid/mail';
+import { MailService, ClientResponse, MailDataRequired } from '@sendgrid/mail';
 
 @Injectable()
 export class SendgridService {
-  constructor(private readonly sendGridMailService: SendGridMailService) {}
+  constructor(private readonly mailService: MailService) {}
 
   async send(
     mail: MailDataRequired
   ): Promise<[ClientResponse, Record<any, any>]> {
     try {
-      const response = await this.sendGridMailService.send(mail);
+      const response = await this.mailService.send(mail);
       return response;
     } catch (error) {
       throw new InternalServerErrorException(error.message);
