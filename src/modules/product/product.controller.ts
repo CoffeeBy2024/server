cfe13627f95eb933fafbf3f295dacb9f8ae12471
timosheyka <kulltima820@gmail.com>
2024-09-25 12:22:45 +0200

    refactor: Product Module Improved according to Review-2

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -19,8 +19,8 @@ import { ApiQuery, ApiTags } from '@nestjs/swagger';
 import { CATEGORY } from '../../common/enums/category.enum';
 import { Product } from './entities/product.entity';
 
-@ApiTags('product')
-@Controller('shop')
+@ApiTags('products')
+@Controller('shops/:id/products')
 export class ProductController {
   constructor(
     private readonly productService: ProductService,
@@ -29,12 +29,12 @@ export class ProductController {
     private readonly shopCategoryService: ShopCategoryService
   ) {}
 
-  @Post('/:id/product')
+  @Post()
   @ApiQuery({
     name: 'category',
     required: false,
     type: 'string',
-    description: 'Category of the shop',
+    description: 'product category',
   })
   async create(
     @Param('id') id: number,
@@ -68,20 +68,16 @@ export class ProductController {
     return this.productService.create(createProductDto, shopCategory);
   }
 
-  @Get('/:sid/product/:id')
-  findOne(@Param('id') id: number) {
-    return this.productService.findOneBy(id);
-  }
-
-  @Get('/:id/products/')
+  @Get()
   @ApiQuery({
     name: 'category',
     required: false,
     type: 'string',
     explode: false,
+    description: 'product category',
   })
   async getCategorySelection(
-    @Param('id') shop_id: number,
+    @Param('id') id: number,
     @Query('category') category?: CATEGORY
   ) {
     if (!category) {
@@ -95,24 +91,29 @@ export class ProductController {
     }
 
     const shopCategory = await this.shopCategoryService.findOneById(
-      shop_id,
+      id,
       categoryEntity
     );
 
     if (!shopCategory) {
-      throw new NotFoundException(`Shop with id ${shop_id} not found`);
+      throw new NotFoundException(`Shop with id ${id} not found`);
     }
 
     return this.productService.findAllByCategory(shopCategory.id);
   }
 
-  @Patch('/:sid/product/:id')
-  update(@Param('id') id: number, @Body() updateProductDto: UpdateProductDto) {
+  @Get('/:pid')
+  findOne(@Param('pid') id: number) {
+    return this.productService.findOneBy(id);
+  }
+
+  @Patch('/:pid')
+  update(@Param('pid') id: number, @Body() updateProductDto: UpdateProductDto) {
     return this.productService.update(id, updateProductDto);
   }
 
-  @Delete('/:sid/product/:id')
-  remove(@Param('id') id: number) {
+  @Delete('/:pid')
+  remove(@Param('pid') id: number) {
     return this.productService.remove(id);
   }
 }
