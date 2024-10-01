70896fdaa341575525314a0dda656f0332914df9
Minchuk Ilya <cacatubutyt@gmail.com>
2024-10-01 22:03:56 +0200

    refactor: hide google callback URL into .env

--- src/modules/auth/strategies/google.strategy.ts
+++ src/modules/auth/strategies/google.strategy.ts
@@ -10,7 +10,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: configService.get('GOOGLE_CLIENT_ID'),
       clientSecret: configService.get('GOOGLE_CLIENT_SECRET'),
-      callbackURL: 'http://localhost:3001/auth/google/redirect',
+      callbackURL: configService.get('GOOGLE_REDIRECT_URL'),
       scope: ['email', 'profile'],
     });
   }
