630983425d341929fe9ba68792e2ab177f3cb429
timosheyka <kulltima820@gmail.com>
2024-10-09 19:13:21 +0200

    perf: added controller to Photo Module for separation of concerns

--- src/modules/photo/photo.module.ts
+++ src/modules/photo/photo.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PhotoService } from './photo.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductPhoto, ShopPhoto } from './entities/photo.entity';
+import { PhotoController } from './photo.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ShopPhoto, ProductPhoto], 'mongodb')],
+  controllers: [PhotoController],
   providers: [PhotoService],
   exports: [PhotoService],
 })
