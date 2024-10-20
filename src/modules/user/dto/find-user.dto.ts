c6985eace429d8684eed159c26a10d33dd08472b
Minchuk Ilya <cacatubutyt@gmail.com>
2024-10-21 00:31:27 +0200

    refactor: explicitly use IsOptional decorator in FindUserDto

--- src/modules/user/dto/find-user.dto.ts
+++ src/modules/user/dto/find-user.dto.ts
@@ -1,10 +1,11 @@
 import { PartialType, PickType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsOptional } from 'class-validator';
 
 export class FindUserDto extends PartialType(
   PickType(CreateUserDto, ['email'] as const)
 ) {
+  @IsOptional()
   @IsNumber()
   readonly id?: number;
 }
