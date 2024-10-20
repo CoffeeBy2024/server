05935264a35bf7fc274f80d273061a2174a0f361
Minchuk Ilya <cacatubutyt@gmail.com>
2024-10-21 00:37:22 +0200

    reactor: use existing dto's in CreateUserDto to avois code duplication

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,46 +1,64 @@
-import { OmitType } from '@nestjs/swagger';
+import {
+  IntersectionType,
+  OmitType,
+  PartialType,
+  PickType,
+} from '@nestjs/swagger';
 import { RegisterUserDto } from '@auth/dto';
 import { Provider } from '@user/entities';
+import { IsBoolean, IsEnum, IsNotEmpty } from 'class-validator';
+import {
+  EmailVerificationDto,
+  PasswordRecoveryVerificationDto,
+} from '@mail/dto';
 
-export class CreateUserDto extends OmitType(RegisterUserDto, [
+const RegisterUserDtoBase = OmitType(RegisterUserDto, [
   'confirmPassword',
   'password',
   'lastName',
-] as const) {
-  lastName?: string;
-  password?: string;
-  provider: Provider;
-  emailVerified: boolean;
-  emailVerificationLink?: string;
-  passwordRecoveryVerificationLink?: string;
+] as const);
 
-  constructor({
-    email,
-    emailVerificationLink,
-    emailVerified,
-    firstName,
-    lastName,
-    password,
-    provider,
-    passwordRecoveryVerificationLink,
-  }: Partial<
-    RegisterUserDto & {
-      provider: Provider;
-      emailVerified: boolean;
-      emailVerificationLink?: string;
-      passwordRecoveryVerificationLink?: string;
-    }
-  >) {
+const PartialRegisterUserDto = PartialType(
+  PickType(RegisterUserDto, ['password', 'lastName'] as const)
+);
+
+const PartialEmailVerificationDto = PartialType(
+  PickType(EmailVerificationDto, ['emailVerificationLink'] as const)
+);
+
+const PartialPasswordRecoveryDto = PartialType(
+  PickType(PasswordRecoveryVerificationDto, [
+    'passwordRecoveryVerificationLink',
+  ] as const)
+);
+
+export class CreateUserDto extends IntersectionType(
+  RegisterUserDtoBase,
+  IntersectionType(
+    PartialRegisterUserDto,
+    IntersectionType(PartialEmailVerificationDto, PartialPasswordRecoveryDto)
+  )
+) {
+  @IsNotEmpty()
+  @IsEnum(Provider)
+  provider: Provider;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  emailVerified: boolean;
+
+  constructor(createUserDto?: CreateUserDto) {
     super();
-    Object.assign(this, {
-      email,
-      emailVerificationLink,
-      emailVerified,
-      firstName,
-      lastName,
-      password,
-      provider,
-      passwordRecoveryVerificationLink,
-    });
+
+    if (createUserDto) {
+      Object.assign(
+        this,
+        Object.fromEntries(
+          Object.entries(createUserDto).filter(
+            ([key]) => key !== 'confirmPassword'
+          )
+        )
+      );
+    }
   }
 }
