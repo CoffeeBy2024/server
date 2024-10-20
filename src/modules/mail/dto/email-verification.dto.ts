612146c01f132b28e748b42b1cb4629a6ef94c31
Minchuk Ilya <cacatubutyt@gmail.com>
2024-10-21 00:38:43 +0200

    refactor: use RegisterUserDto for PasswordRecoveryVerificationDto and EmailVerificationDto to avoid code duplication

--- src/modules/mail/dto/email-verification.dto.ts
+++ src/modules/mail/dto/email-verification.dto.ts
@@ -1,10 +1,10 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { RegisterUserDto } from '@auth/dto';
+import { PickType } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class EmailVerificationDto {
-  @IsNotEmpty()
-  @IsEmail()
-  email: string;
-
+export class EmailVerificationDto extends PickType(RegisterUserDto, [
+  'email',
+] as const) {
   @IsNotEmpty()
   @IsString()
   emailVerificationLink: string;
--- src/modules/mail/dto/password-recovery-verification.dto.ts
+++ src/modules/mail/dto/password-recovery-verification.dto.ts
@@ -1,10 +1,10 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { RegisterUserDto } from '@auth/dto';
+import { PickType } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class PasswordRecoveryVerificationDto {
-  @IsNotEmpty()
-  @IsEmail()
-  email: string;
-
+export class PasswordRecoveryVerificationDto extends PickType(RegisterUserDto, [
+  'email',
+] as const) {
   @IsNotEmpty()
   @IsString()
   passwordRecoveryVerificationLink: string;
