0b4c037d8638f1de40d55661580d4dedb172d994
Minchuk Ilya <cacatubutyt@gmail.com>
2024-10-21 20:00:39 +0200

    refactor: remove redundant MailController

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SendgridModule } from './sendgrid/sendgrid.module';
 import { MailService } from './mail.service';
-import { MailController } from './mail.controller';
 
 @Module({
   imports: [SendgridModule, ConfigModule],
-  controllers: [MailController],
   providers: [MailService],
   exports: [MailService],
 })
