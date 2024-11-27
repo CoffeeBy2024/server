6a361a1f8eb9fa39b20679cad8b08300256e959a
im <cacatubutyt@gmail.com>
2024-11-27 12:04:52 +0100

    Merge branch 'develop' into feature/reset-psw

--- src/modules/shop/shop/shop.controller.ts
+++ src/modules/shop/shop/shop.controller.ts
@@ -127,7 +127,7 @@ export class ShopController {
     @UploadedFile(
       new ParseFilePipeBuilder()
         .addFileTypeValidator({ fileType: /(jpg|jpeg|png|webp)$/ })
-        .build()
+        .build({ fileIsRequired: false })
     )
     file?: Express.Multer.File
   ) {
