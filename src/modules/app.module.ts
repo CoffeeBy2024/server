1402cea3c7badf6c16d8f7133dd35fdb6a5a54c5
im <cacatubutyt@gmail.com>
2024-11-27 14:18:33 +0100

    fix: remove import duplication in app.module.ts

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -21,7 +21,6 @@ import { TTLVariables } from 'src/utils/constants/cache';
 import config from 'src/config/dbconfig';
 import { JwtAuthGuard } from '@auth/guards';
 import { CustomCacheInterceptor } from '@common/interceptors';
-import config from 'src/config/dbconfig';
 
 @Module({
   imports: [
